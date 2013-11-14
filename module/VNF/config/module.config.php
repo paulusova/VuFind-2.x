6f5cb2fe41cfc0dc90ec4be28e96fc4898fe60ca
Vaclav Rosecky <xrosecky@gmail.com>
2013-11-14 11:32:19 +0100

    record drivers for institutions

--- module/VNF/config/module.config.php
+++ module/VNF/config/module.config.php
@@ -2,27 +2,66 @@
 namespace VNF\Module\Configuration;
 
 $config = array(
-    'vufind' => array(
-	    'plugin_managers' => array (
-			'recorddriver' => array (
-				'factories' => array(
-					'solrmkp' => function ($sm) {
-					    $driver = new \VNF\RecordDriver\SolrMkp(
-					    		$sm->getServiceLocator()->get('VuFind\Config')->get('config'),
-					    		null,
-					    		$sm->getServiceLocator()->get('VuFind\Config')->get('searches')
-					    );
-					    $driver->attachILS(
-					    		$sm->getServiceLocator()->get('VuFind\ILSConnection'),
-					    		$sm->getServiceLocator()->get('VuFind\ILSHoldLogic'),
-					    		$sm->getServiceLocator()->get('VuFind\ILSTitleHoldLogic')
-					    );
-					    return $driver;
-					} /* solrmkp */
-				) /* factories */
-			) /* recorddriver */
-		) /* plugin_managers */
-	) /* vufind */
+   'vufind' => array(
+      'plugin_managers' => array (
+         'recorddriver' => array (
+            'factories' => array(
+               'solrmkp' => function ($sm) {
+                  $driver = new \VNF\RecordDriver\SolrMkp(
+                      $sm->getServiceLocator()->get('VuFind\Config')->get('config'),
+                      null,
+                      $sm->getServiceLocator()->get('VuFind\Config')->get('searches')
+				   );
+                   $driver->attachILS(
+                      $sm->getServiceLocator()->get('VuFind\ILSConnection'),
+                      $sm->getServiceLocator()->get('VuFind\ILSHoldLogic'),
+                      $sm->getServiceLocator()->get('VuFind\ILSTitleHoldLogic')
+                   );
+                   return $driver;
+               }, /* solrmkp */
+               'solrcbvk' => function ($sm) {
+                  $driver = new \VuFind\RecordDriver\SolrMarc(
+                     $sm->getServiceLocator()->get('VuFind\Config')->get('config'),
+                     null,
+                     $sm->getServiceLocator()->get('VuFind\Config')->get('searches')
+                  );
+                  $driver->attachILS(
+                     $sm->getServiceLocator()->get('VuFind\ILSConnection'),
+                     $sm->getServiceLocator()->get('VuFind\ILSHoldLogic'),
+                     $sm->getServiceLocator()->get('VuFind\ILSTitleHoldLogic')
+                  );
+                  return $driver;
+               }, /* solrcbvk */
+               'solrmzk' => function ($sm) {
+                  $driver = new \VuFind\RecordDriver\SolrMarc(
+                     $sm->getServiceLocator()->get('VuFind\Config')->get('config'),
+                     null,
+                     $sm->getServiceLocator()->get('VuFind\Config')->get('searches')
+                  );
+                  $driver->attachILS(
+                     $sm->getServiceLocator()->get('VuFind\ILSConnection'),
+                     $sm->getServiceLocator()->get('VuFind\ILSHoldLogic'),
+                     $sm->getServiceLocator()->get('VuFind\ILSTitleHoldLogic')
+                  );
+                  return $driver;
+               }, /* solrmzk */
+               'solrvnf' => function ($sm) {
+                  $driver = new \VuFind\RecordDriver\SolrMarc(
+                     $sm->getServiceLocator()->get('VuFind\Config')->get('config'),
+                     null,
+                     $sm->getServiceLocator()->get('VuFind\Config')->get('searches')
+                  );
+                  $driver->attachILS(
+                     $sm->getServiceLocator()->get('VuFind\ILSConnection'),
+                     $sm->getServiceLocator()->get('VuFind\ILSHoldLogic'),
+                     $sm->getServiceLocator()->get('VuFind\ILSTitleHoldLogic')
+                  );
+                  return $driver;
+               }, /* solrvnf */
+            ) /* factories */
+         ) /* recorddriver */
+      ) /* plugin_managers */
+   ) /* vufind */
 );
 
 return $config;
